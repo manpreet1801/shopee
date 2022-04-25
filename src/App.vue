a617f0fbc5ecbadf6639c0f91de71ae51c52570a
Manpreet Singh <manpreetsinghs8382@gmail.com>
2022-04-25 12:04:42 +0530

    initial setupt

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <shopee/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import shopee from './components/shopee.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    shopee
   }
 }
 </script>
