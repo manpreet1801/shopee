4c987ab440317cbb835d418792748ba579632b3e
@rutikaparkhe <rutika.p@olcademy.in>
2022-04-26 12:39:03 +0530

    shopping cart UI design

---
+++ src/components/ShoppingCart/rightSide.vue
@@ -0,0 +1,96 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  /* display: flex; */
+  height: 100%;
+  width: 50%;
+}
+.header {
+  color: #000000;
+  font-family: "DM Sans";
+  font-style: normal;
+}
+.details {
+  margin-left: 0;
+  margin-right: 0;
+  margin-top: 0;
+  margin-bottom: auto;
+  /* margin: auto; */
+  width: max-content;
+  padding-left: 16px;
+  font-family: "DM Sans";
+  font-style: normal;
+}
+.price {
+  padding-top: 8px;
+}
+.img {
+  margin-left: 0;
+  margin-right: 0;
+  margin-top: auto;
+  margin-bottom: auto;
+  padding-right: 16px;
+}
+.sub-container {
+  display: flex;
+  padding-bottom: 10px;
+  padding-top: 10px;
+}
+.split {
+  height: 100%;
+  width: 50%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+.item-name {
+  display: flex;
+  font-weight: bold;
+  margin-bottom: 0.9rem;
+}
+.size {
+  margin-left: 0;
+}
+.price {
+  display: flex;
+  color: #a18a68;
+}
+.logo {
+  height: 100px;
+  width: 100px;
+}
+/* .mainBtn{
+    margin-left: 160px;
+} */
+.btn {
+  border: none;
+  padding-top: 2px;
+  margin: 4px;
+  padding-bottom: 2px;
+}
+.quantity {
+  padding-left: 8.4rem;
+}
+.delete {
+  padding-left: 2rem;
+}
+.cross-sign {
+  height: 12px;
+  width: 11.82px;
+}
+</style>
---
+++ src/components/ShoppingCart/shoppingcart.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <h2 class="header">Shopping cart</h2>
+    <div class="container left">
+      <leftSide />
+    </div>
+    <div class="container right">
+      <rightSide />
+    </div>
+  </div>
+</template>
+
+<script>
+import leftSide from "./leftSide.vue";
+import rightSide from "./rightSide.vue";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  components: {
+    leftSide,
+    rightSide,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.right{
+  right: 0;
+}
+.left {
+  left: 0;
+}
+.container {
+  /* top: 0; */
+  display: flex;
+  height: 100%;
+  width: 50%;
+  position: fixed;
+  z-index: 1;
+  overflow-x: hidden;
+  padding-top: 20px;
+  padding-left: 40px;
+}
+.header {
+  color: #000000;
+  font-family: "DM Sans";
+  font-style: normal;
+}
+.details {
+  margin-left: 0;
+  margin-right: 0;
+  margin-top: 0;
+  margin-bottom: auto;
+  /* margin: auto; */
+  width: max-content;
+  padding-left: 16px;
+  font-family: "DM Sans";
+  font-style: normal;
+}
+.price {
+  padding-top: 8px;
+}
+.img {
+  margin-left: 0;
+  margin-right: 0;
+  margin-top: auto;
+  margin-bottom: auto;
+  padding-right: 16px;
+}
+.sub-container {
+  display: flex;
+  padding-bottom: 10px;
+  padding-top: 10px;
+}
+.split {
+  height: 100%;
+  width: 50%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+.item-name {
+  display: flex;
+  font-weight: bold;
+  margin-bottom: 0.9rem;
+}
+.size {
+  margin-left: 0;
+}
+.price {
+  display: flex;
+  color: #a18a68;
+}
+.logo {
+  height: 100px;
+  width: 100px;
+}
+/* .mainBtn{
+    margin-left: 160px;
+} */
+.btn {
+  border: none;
+  padding-top: 2px;
+  margin: 4px;
+  padding-bottom: 2px;
+}
+.quantity {
+  padding-left: 8.4rem;
+}
+.delete {
+  padding-left: 2rem;
+}
+.cross-sign {
+  height: 12px;
+  width: 11.82px;
+}
+</style>
