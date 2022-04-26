5a21af420f3344e93535fe5702b4e4bd9b93d4cb
Manpreet Singh <manpreetsinghs8382@gmail.com>
2022-04-26 11:15:14 +0530

    footer home

---
+++ src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="divider">
+      <img src="../assets/divider.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterView",
+};
+</script>
+
+<style scoped>
+.divider{
+    
+
+}
+
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <img src="../assets/Img1.svg" alt="" />
+    <div class="view">
+      <div id="label">Shop The Latest</div>
+      <div>View all</div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "./footer.vue";
+export default {
+  name: "homePage",
+  components: {
+    Footer,
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  margin: 10px 0px 0px 96px;
+}
+#label {
+  font-weight: 600;
+  font-size: 33px;
+}
+.view {
+  justify-content: space-between;
+  margin-top: 60px;
+  display: flex;
+}
+</style>
