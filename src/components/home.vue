7fc1cc88752faa5b87c7732d0ef81e0942e08051
Manpreet Singh <manpreetsinghs8382@gmail.com>
2022-04-26 15:17:29 +0530

    footer

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="home">
-    <img src="../assets/Img1.svg" alt="" />
+    <img class="img" src="../assets/Img1.svg" alt="" />
+    <div class="product-label">
+      <div id="labelName">Gold Big Hoops</div>
+      <div id="price">$68.00</div>
+      <button class="button">View Product</button>
+    </div>
     <div class="view">
       <div id="label">Shop The Latest</div>
-      <div>View all</div>
+      <a href="" id="viewAll">View All</a>
     </div>
     <Footer />
   </div>
@@ -21,7 +26,7 @@ export default {
 
 <style scoped>
 .home {
-  margin: 10px 0px 0px 96px;
+  /* margin: 10px 0px 0px 96px; */
 }
 #label {
   font-weight: 600;
@@ -31,5 +36,34 @@ export default {
   justify-content: space-between;
   margin-top: 60px;
   display: flex;
+}
+.product-label {
+  position: absolute;
+  top: 200px;
+  left: 150px;
+  color: white;
+}
+.button {
+  border: 2px solid white;
+  border-radius: 5px;
+  color: white;
+  background-color: transparent;
+  margin-top: 3rem;
+  font-size: 20px;
+  padding: 13px 30px 14px 30px;
+}
+#labelName {
+  font-size: 3rem;
+}
+#price {
+  font-size: 2rem;
+  margin-top: 10px;
+}
+#viewAll {
+  color: #a18a68;
+  border: none;
+}
+.img {
+  width: 100%;
 }
 </style>
