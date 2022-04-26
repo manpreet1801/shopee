8273233b184c4d3f79fde747075cb0ba4e426451
Manpreet Singh <manpreetsinghs8382@gmail.com>
2022-04-26 16:25:26 +0530

    changes

--- src/components/home.vue
+++ src/components/home.vue
@@ -8,18 +8,89 @@
     </div>
     <div class="view">
       <div id="label">Shop The Latest</div>
+
       <a href="" id="viewAll">View All</a>
+    </div>
+    <div class="product-list">
+      <productCard
+        v-for="product in products"
+        :product="product"
+        :key="product.id"
+      />
     </div>
     <Footer />
   </div>
 </template>
 
 <script>
+import prod1 from "../assets/product-1.svg";
+import prod2 from "../assets/product-2.svg";
+import prod3 from "../assets/product-3.svg";
+import prod4 from "../assets/product-4.svg";
+import prod5 from "../assets/product-5.svg";
+import prod6 from "../assets/product-6.svg";
 import Footer from "./footer.vue";
+import productCard from "./productCard.vue";
+
 export default {
   name: "homePage",
   components: {
     Footer,
+    productCard,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: "p1",
+          name: "Lira Earrings",
+          price: 20.0,
+          isInStock: true,
+          image: prod1,
+          showLabel: false,
+        },
+        {
+          id: "p2",
+          name: "Hal Earrings",
+          price: 25.0,
+          isInStock: true,
+          image: prod2,
+          showLabel: false,
+        },
+        {
+          id: "p3",
+          name: "Kaede Hair Pin Set Of 3",
+          price: 30.0,
+          isInStock: true,
+          image: prod3,
+          showLabel: false,
+        },
+        {
+          id: "p4",
+          name: "Hair Pin Set Of 3",
+          price: 30.0,
+          isInStock: true,
+          image: prod4,
+          showLabel: false,
+        },
+        {
+          id: "p5",
+          name: "Plaine Necklace",
+          price: 19.0,
+          isInStock: true,
+          image: prod5,
+          showLabel: false,
+        },
+        {
+          id: "p6",
+          name: "Yuki Hair Pin Set of 3",
+          price: 29.0,
+          isInStock: false,
+          image: prod6,
+          showLabel: false,
+        },
+      ],
+    };
   },
 };
 </script>
@@ -27,6 +98,7 @@ export default {
 <style scoped>
 .home {
   /* margin: 10px 0px 0px 96px; */
+  width: 100%;
 }
 #label {
   font-weight: 600;
@@ -42,6 +114,13 @@ export default {
   top: 200px;
   left: 150px;
   color: white;
+}
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+  flex: 1;
+  gap: 1.25rem;
+  margin-top: 30px;
 }
 .button {
   border: 2px solid white;
--- src/components/productCard.vue
+++ src/components/productCard.vue
@@ -1,53 +1,63 @@
 <template>
-    <div class="product-wrapper">
-        <div class="product-img-container">
-            <img class="product-image" :src="product.image" />
-        </div>
-        <div class="product-label product-discount" v-if="product.hasDiscount">-%{{ product.discount }}</div>
-        <div class="product-label product-stock" v-if="!product.isInStock">Sold out</div>
-        <div class="product-name">
-            <h3>{{ product.name }}</h3>
-        </div>
-        <div class="product-price">
-            <h4>$ {{ product.price }}</h4>
-        </div>
+  <div class="product-wrapper">
+    <div class="product-img-container">
+      <img class="product-image" :src="product.image" />
     </div>
+    <div
+      class="product-label product-discount"
+      v-if="product.showLabel && product.hasDiscount"
+    >
+      -%{{ product.discount }}
+    </div>
+    <div
+      class="product-label product-stock"
+      v-if="product.showLabel && !product.isInStock"
+    >
+      Sold out
+    </div>
+    <div class="product-name">
+      <h3>{{ product.name }}</h3>
+    </div>
+    <div class="product-price">
+      <h4>$ {{ product.price }}</h4>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['product'],
+  props: ["product"],
 };
 </script>
 
 <style scoped>
 .product-wrapper {
-    position: relative;
+  position: relative;
 }
 .product-img-container {
-    margin-bottom: 1.5rem;
+  margin-bottom: 1.5rem;
 }
 .product-label {
-    background-color: #a18a68;
-    padding: 2px 8px;
-    border-radius: 4px;
-    color: #fff;
-    position: absolute;
-    left: 16px;
-    top: 16px;
+  background-color: #a18a68;
+  padding: 2px 8px;
+  border-radius: 4px;
+  color: #fff;
+  position: absolute;
+  left: 16px;
+  top: 16px;
 }
 .product-name h3 {
-    font-size: 1.25rem;
-    font-weight: 400;
-    line-height: 26px;
-    margin-bottom: 1rem;
+  font-size: 1.25rem;
+  font-weight: 400;
+  line-height: 26px;
+  margin-bottom: 1rem;
 }
 .product-price h4 {
-    font-style: normal;
-    font-weight: 500;
-    font-size: 1.25rem;
-    line-height: 26px;
-    text-transform: capitalize;
-    color: #a18a68;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.25rem;
+  line-height: 26px;
+  text-transform: capitalize;
+  color: #a18a68;
 }
 </style>
